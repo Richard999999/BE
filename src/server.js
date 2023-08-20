6b8fd9acb2b60d155d281a2e7e3e8366c40ca4bc
Richard999999 <ramaandhika618@gmail.com>
2023-08-20 17:33:12 +0700

    postgres database implemented

--- src/server.js
+++ src/server.js
@@ -1,13 +1,17 @@
+/* eslint-disable import/no-extraneous-dependencies */
+// mengimpor dotenv dan menjalankan konfigurasinya
+require('dotenv').config();
+
 const Hapi = require('@hapi/hapi');
 const notes = require('./api/notes');
-const NotesService = require('./services/inMemory/NotesService');
 const NotesValidator = require('./validator/notes');
+const NotesServiceP = require('./services/postgres/NotesServiceP');
 
 const init = async () => {
-  const notesService = new NotesService();
+  const notesService = new NotesServiceP();
   const server = Hapi.server({
-    port: 5000,
-    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    port: process.env.PORT,
+    host: process.env.HOST,
     routes: {
       cors: {
         origin: ['*'],
